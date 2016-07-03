78d87e218de70bc5e16a493e0bf087addbf82627
dxr3977 <dxr3977@g.rit.edu>
2016-07-03 10:19:45 -0400

    Only SudokuConfig left to be implemented.

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,5 +1,40 @@
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
- * Created by Lord Daniel on 7/3/2016.
+ * The representation of a single configuration for a puzzle.
+ * The backtracking.Backtracker depends on these routines in order to
+ * solve a puzzle.  Therefore, all puzzles must implement this
+ * interface.
+ *
+ * @author Sean Strout @ RIT CS
  */
-public class Configuration {
+public interface Configuration {
+    /**
+     * Get the collection of successors from the current one.
+     *
+     * @return All successors, valid and invalid
+     */
+    public Collection< Configuration > getSuccessors();
+
+    /**
+     * Is the current configuration valid or not?
+     *
+     * @return true if valid; false otherwise
+     */
+    public boolean isValid();
+
+    /**
+     * Is the current configuration a goal?
+     * @return true if goal; false otherwise
+     */
+    public boolean isGoal();
+
+    /**
+     * calls toString
+     */
+    public void display();
+
 }
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,5 +1,32 @@
+import java.io.FileNotFoundException;
+import java.util.Optional;
+
 /**
  * Created by Lord Daniel on 7/3/2016.
  */
 public class SudokuSolver {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // construct the initial configuration from the file
+        Configuration init = new SudokuConfig(args[0]);
+
+        Backtracker bt = new Backtracker(false);
+
+        // start the clock
+        double start = System.currentTimeMillis();
+
+        // attempt to solve the puzzle
+        Optional<Configuration> sol = bt.solve(init);
+
+        // compute the elapsed time
+        System.out.println("Elapsed time: " +
+                (System.currentTimeMillis() - start)/1000.0 + " seconds.");
+
+        // indicate whether there was a solution, or not
+        if (sol.isPresent()) {
+            System.out.println("SOLVED");
+        } else {
+            System.out.println("No solution!");
+        }
+    }
 }
